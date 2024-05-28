e531b011157a2272f4fb5e6f4d1d71c072c30161
RetroGrinco <ciampad05@gmail.com>
2024-05-28 14:35:36 +0200

    Add some content and style  in homepage

--- angularjs/routing/src/app/app-routing.module.ts
+++ angularjs/routing/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component'
+import { ContactsComponent } from './components/contacts/contacts.component';
+import { AboutComponent } from './components/about/about.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component: HomeComponent},
+  {path:'about', component: AboutComponent},
+  {path:'contact', component: ContactsComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- angularjs/routing/src/app/app.module.ts
+++ angularjs/routing/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './components/about/about.component';
+import { ContactsComponent } from './components/contacts/contacts.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    ContactsComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
