3cb6230c80445f38eed1602adde32f2676927790
RetroGrinco <ciampad05@gmail.com>
2024-05-29 17:03:31 +0200

    Add content for About and Contact page

--- angularjs/routing/src/app/components/contacts/contacts.component.ts
+++ angularjs/routing/src/app/components/contacts/contacts.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrl: './contacts.component.css'
 })
 export class ContactsComponent {
-
+  people = [
+    {name: "Davide", surname: "Ciampa", phone: 33311111111},
+    {name: "Silvia", surname: "Setti", phone: 33311111111},
+    {name: "Valentina", surname: "Secchi", phone: 33311111111} ,
+    {name: "Daniele", surname: "Pirina", phone: 33311111111},
+    {name: "Davide", surname: "Meloni", phone: 33311111111},
+  ]
 }
