44686a42a94cdbfa71ec6702144b4e5318eabd71
RetroGrinco <168284136+RetroGrinco@users.noreply.github.com>
2024-08-06 15:42:48 +0200

    Delete angularjs directory

--- angularjs/parentChild_childParent/src/app/direttive/action.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { SomeActionDirective } from './action.directive';
-
-describe('SomeActionDirective', () => {
-  it('should create an instance', () => {
-    const directive = new SomeActionDirective();
-    expect(directive).toBeTruthy();
-  });
-});
